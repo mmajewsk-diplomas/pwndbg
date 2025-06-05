16c75e6c24f1343496ae00e6a6ada559fbfc5883
jxuanli <65455765+jxuanli@users.noreply.github.com>
2025-06-05 06:12:11 -0700

    Improving `vmmap` output when debugging kernel (#3020)
    * page walks
    
    * Improving kernel-vmmap
    
    * added test for kernel vmmap
    
    * improve userland heap handling
    
    * improve kernelland sections handling
    
    * fixed typo
    
    * fixed test
    
    * adding support for info mem
    
    * changed array to tuple based on suggestions
    
    * removing esp fixup stacks from display
    
    * including call stacks
    
    * implemented pagewalk
    
    * added pagewalk test
    
    * improved testing / output
    
    * added docs
    
    * renaming
    
    * actually adding the remamed file
    
    * adding decoration for cpu arch
    
    * Revert "adding decoration for cpu arch"
    
    This reverts commit 84aa120f688d50a49868fde6de0b3d3276e23ce4.
    
    * added arch check for pagewalk
    
    * adding req on symbols
    
    * supporting mem info
    
    * refactored pagewalk helpers
    
    * added support for older versions of qemu-x86_64
    
    * improved pagewalk helper function signature
    
    * improved processing of vmmap
    
    * refactored a bit more
    
    * refactored a bit more
    
    * removed changes to kernel/vmmap
    
    * adding option to not process pages
    
    * improving support to info mem
    
    * changed to tuple
    
    * changed to tuple
    
    * changed to tuple
    
    * added aarch64 mem mapping
    
    * improved testing
    
    * fixing for arm64
    
    * a bit more amending
    
    * fixing test

--- pwndbg/aglib/kernel/paging.py
+++ pwndbg/aglib/kernel/paging.py
@@ -15,7 +15,7 @@ ENTRYMASK = ~((1 << 12) - 1) & ((1 << 51) - 1)
 
 @pwndbg.lib.cache.cache_until("start", "stop")
 def get_memory_map_raw() -> Tuple[pwndbg.lib.memory.Page, ...]:
-    return pwndbg.aglib.kernel.vmmap.kernel_vmmap()
+    return pwndbg.aglib.kernel.vmmap.kernel_vmmap(False)
 
 
 def find_kbase(pages) -> int | None:
@@ -32,7 +32,7 @@ def find_kbase(pages) -> int | None:
 
     mappings = pages
     for mapping in mappings:
-        # TODO: Check alignment
+        # should be page aligned -- either from pt-dump or info mem
 
         # only search in kernel mappings:
         # https://www.kernel.org/doc/html/v5.3/arm64/memory.html
@@ -68,11 +68,11 @@ guess_physmap = config.add_param(
 
 
 def physmap_base() -> int:
-    if pwndbg.aglib.kernel.has_debug_syms():
+    if pwndbg.aglib.kernel.has_debug_syms() and pwndbg.aglib.arch.name == "x86-64":
         result = pwndbg.aglib.symbol.lookup_symbol_value("page_offset_base")
         if result is not None:
             return result
-    if guess_physmap:
+    if guess_physmap or pwndbg.aglib.arch.name == "aarch64":
         result = guess_physmap_base()
         if result is not None:
             return result
