4891cceafebebbc7d5814157703187f6d4f25c2d
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-08-28 00:51:56 +0200

    Add max-decimal-number config (#3259)
    * centralize max-decimal deteminration and add config
    
    * add comments about consistency with capstone
    
    * fix tests
    
    * drive-by fix

--- pwndbg/lib/pretty_print.py
+++ pwndbg/lib/pretty_print.py
@@ -5,9 +5,67 @@ from typing import Any
 from typing import Callable
 from typing import List
 from typing import Optional
+from typing import Tuple
 
+import pwndbg
 import pwndbg.color as color
 from pwndbg.color import theme
+
+max_decimal_number = pwndbg.config.add_param(
+    "max-decimal-number",
+    9,
+    "show all numbers greater than this in hex",
+    param_class=pwndbg.lib.config.PARAM_ZUINTEGER_UNLIMITED,
+    help_docstring="""
+For negative numbers, their absolute value is used.
+
+Set the parameter to 'unlimited' if you want all values in decimal.
+Specially, set the parameter to zero if you want all values in hex.
+
+The assembly instruction operands come from capstone, and are thus
+not controlled by this setting. For consistency with them, leave
+this setting at 9 (the default).
+""",
+# We could look into also overwriting the capstone operands string, similarly
+# to what is done here: https://github.com/pwndbg/pwndbg/blob/26db4533aa08d77c4bbc359b4760a0944e0c6b23/pwndbg/aglib/disasm/arch.py#L322-L331
+)
+
+
+def int_to_string(num: int) -> str:
+    """
+    Converts an integer value to string.
+
+    Decides whether to format it in decimal or
+    hex depending on the max-decimal-number config.
+    """
+    if max_decimal_number == -1:
+        return f"{num}"
+    elif max_decimal_number == 0:
+        return f"{num:#x}"
+    elif abs(num) > max_decimal_number:
+        return f"{num:#x}"
+    else:
+        return f"{num}"
+
+
+def int_pair_to_string(num1: int, num2: int) -> Tuple[str, str]:
+    """
+    Converts an integer pair to a string pair.
+
+    Decides whether to format them in decimal or
+    hex depending on the max-decimal-number config.
+
+    If either value should be hex, both are hex.
+    """
+    if max_decimal_number == -1:
+        return f"{num1}", f"{num2}"
+    elif max_decimal_number == 0:
+        return f"{num1:#x}", f"{num2:#x}"
+    elif abs(num1) > max_decimal_number or abs(num2) > max_decimal_number:
+        return f"{num1:#x}", f"{num2:#x}"
+    else:
+        return f"{num1}", f"{num2}"
+
 
 config_property_name_color = theme.add_color_param(
     "prop-name-color",
@@ -35,6 +93,7 @@ config_property_title_color = theme.add_color_param(
 Used heavily in mallocng commands.
 """
 )
+
 
 @dataclass
 class Property:
@@ -129,15 +188,10 @@ def from_properties(
     # Transform prop values to string representation
     for prop in properties:
         if isinstance(prop.value, int):
-            if prop.use_hex:
-                prop.value = hex(prop.value)
-            else:
-                prop.value = str(prop.value)
+            prop.value = f"{prop.value:#x}" if prop.use_hex else f"{prop.value}"
+
         if isinstance(prop.alt_value, int):
-            if prop.use_hex:
-                prop.alt_value = hex(prop.alt_value)
-            else:
-                prop.alt_value = str(prop.alt_value)
+            prop.alt_value = f"{prop.alt_value:#x}" if prop.use_hex else f"{prop.alt_value}"
 
     indentation_str = indent_size * " "
     extra_list_pad_str = (
